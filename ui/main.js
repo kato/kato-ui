2e0bb045c979cc2f148abf0ccfa8257163f54816
李雨田 <demon@danwi.me>
2018-12-07 21:44:20 +0800

    完善打包,并添加了element-ui

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import {getAPIClient} from "./plugins/kato-client";
+
+//导入主样式
+import './main.scss'
 
 Vue.config.productionTip = false;
 
@@ -9,6 +13,9 @@ Vue.config.productionTip = false;
   //引入kato-client,api的baseurl在ui的上一层
   const apiUrl = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/ui')) + '/';
   Vue.use(await getAPIClient(apiUrl));
+
+  //引入element-ui
+  Vue.use(ElementUI);
 
   //初始化Kato API插件
   new Vue({
